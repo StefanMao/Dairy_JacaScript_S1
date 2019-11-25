93c55cead37012bfbc4bc9668614dee292f67de7
maochichi <stefanmao1112@gmail.com>
2019-11-26 00:01:22 +0800

    no message

--- bootstrap/main.js
+++ bootstrap/main.js
@@ -1,3 +1,21 @@
+
 $(document).ready(function(){
+
  $('#datepicker').datepicker(); 
+
+ //上傳圖片
+ $("#profile-img").change(function(){
+       readURL(this);
+  });
 });
+
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            
+            reader.onload = function (e) {
+                $('#profile-img-tag').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
