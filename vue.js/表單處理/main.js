4196fed5c7eebaf7322fb6623e09150e69682ae4
maochichi <stefanmao1112@gmail.com>
2019-12-06 16:54:52 +0800

    no message

---
+++ vue.js/表單處理/main.js
@@ -0,0 +1,17 @@
+(function (){
+
+    let data={
+        name:'Mao Mao',
+        sex:'男',
+        age:'20歲-30歲間',
+        favor:['打工','電玩','遊日本'],
+        member:true
+    }
+
+    let vm =new Vue ({
+        el:'#app',
+        data : data
+    })
+
+
+}())
