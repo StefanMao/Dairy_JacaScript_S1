4d668c7ab4fa4cc2b1d72b5ba016ac55268bbcb8
maochichi <stefanmao1112@gmail.com>
2019-12-16 16:55:58 +0800

    vue.js 串接 Api  [ CRUD] 功能

--- vue.js/main.js
+++ vue.js/main.js
@@ -1,42 +1,108 @@
-(function (){
+;(function (Vue){
 
+    let book =[]
+    new Vue({
 
-
- let data ={
-
-    input:{
-        type:'工程',
-        title:''
-    },
-    menu:[
-        {type : '工程' ,title:' Big JAVA BOOK' , link:'javascript:;'},
-        {type : '文學' ,title:' 情商課' , link:'javascript:;'},
-        {type : '哲學' ,title:'蘇菲的世界' , link:'javascript:;'},
-        {type : '兒童故事書' ,title:'格林童話' , link:'javascript:;'},
-        {type : '語言' ,title:'多益單字本' , link:'javascript:;'}
-    ]
- }
-
- let vm = new Vue ({
-
-    el:'#app',
-    data: data,
-    methods:{
-        inputHandler(){
-            
-            if(this.input.title)
-            {
-                this.menu.push({
-                    type:this.input.type,
-                    title:this.input.title,
-                    link:'javascript:;'
-                })
-                this.input.title=''
+        el:"#app",
+        data:{
+            loading:false,
+            Book:[],
+            input:{
+                BookType:"全部",
+                BookName:"",
+                Author:"",  
             }
+        },
+        methods:{
+            SubmitHander(){
+                //解構賦值
+                let {BookType,BookName,Author}=this.input
+                if( !BookType || !BookName ||!Author){
+                    alert("不可有欄位空白!!")
+                    return 
+                }
+                loading=true
+                axios.post('http://localhost:8888/Book',this.input).then((res)=>{
+                    this.Book.push(res.data)
+                    this.CancleHander()
+                    loading=false
+                    
+                }).catch((err)=>{
+                    console.log(err)
+                })
+
+
+            },
+            CancleHander(){
+               this.input.BookType='全部'
+               this.input.BookName=''
+               this.input.Author=''
+            }
+        },
+        mounted(){
+            this.loading=true
+            axios.get('http://localhost:8888/Book')
+            .then((res)=>{
+                console.log(res)
+                this.Book=res.data
+                this.loading=false
+            }).catch((err)=>{
+                console.log(err)
+            })
+
         }
 
-    }
+    })
 
- })
 
-}())
+
+
+}(Vue))
+
+
+//  let vm = new Vue ({
+
+//     el:'#app',
+//     data: data,
+//     methods:{
+//         inputHandler(){
+            
+//             if(this.input.title)
+//             {
+//                 this.menu.push({
+//                     type:this.input.type,
+//                     title:this.input.title,
+//                     link:'javascript:;'
+//                 })
+//                 this.input.title=''
+//             }
+//         }
+//     },
+//     computed:{
+//         typemenu(){
+//             if(this.input.type !== '全部'){
+//                 return this.menu.filter(item =>{
+//                     return item.type == this.input.type
+//                 })               
+//             }else{
+//                 return this.menu  
+//             }
+//         },
+//         titlemenu(){
+//             if(this.input.title){
+//               return this.typemenu.filter(item => {
+//               let content = item.title.toLowerCase()
+//               let keyword = this.input.title.toLowerCase()
+//               return content.indexOf(keyword) != -1
+//             })
+//             }else{
+//                 return this.typemenu
+//             }
+//         }
+//     }
+
+
+    
+
+//  })
+
